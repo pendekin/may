cdf35d26ecab524fc1b472e294b4399cc0bf61bc
pendekin <80629492+pendekin@users.noreply.github.com>
2022-04-09 10:54:47 +0700

    Create Toyib.js

---
+++ Toyib.js
@@ -0,0 +1,2 @@
+if(window.location.href.indexOf("#?o=") != -1){ document.getElementById('adsByDC').innerHTML="<div id='timer'/><div style='text-align: center'><button class='bt-success hidden' disabled='' id='wcGetLink'>Get Link</button></div>";document.getElementById('adsByDC2').innerHTML="<div style='text-align: center'><button class='bt-success hidden' disabled='' id='gotolink'>Gotolink</button></div>";
+}
